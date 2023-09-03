bd81b467821f53d158b1abb16c67ac233d663309
Gokhan Canbolat <108427140+gokhanncanbolat@users.noreply.github.com>
2023-09-03 13:16:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int deger = 2;
+
+string name = null;
+string surname = " ";
+
+byte s = 5;
+sbyte a = 127;
+
+short b = 263;
+ushort c = 65365;
+
+Int32 i32 = 2;
+
+float f = 5;
+double d = 3.0;
+
+char ch = 'c';
+
+object o1 = "x";
+
+
